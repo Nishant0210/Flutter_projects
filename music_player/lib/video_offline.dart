8ac14d12a42b238ea3a1f21be3ed8310376dfdb4
Shubham Shinde <57897574+shubhamshinde4141@users.noreply.github.com>
2021-10-04 10:21:06 +0530

    Update video_offline.dart

--- music_player/lib/video_offline.dart
+++ music_player/lib/video_offline.dart
@@ -23,7 +23,7 @@ class VideoState extends State<PlayVideo> {
     super.initState();
     playerController = VideoPlayerController.asset("assets/videos/UddGaye.mp4")
       ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        // Ensure the first frame is shown after the video is initialized, even before play button has been pressed.
         setState(() {});
       });
   }
